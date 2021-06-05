6b51cf4a2da598192df3278a92775e07e5d9fefd
shreyansh123-12123 <84223625+shreyansh123-12123@users.noreply.github.com>
2021-06-05 10:26:07 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,5 @@
+function send(){
+    number1 = document.getElementById ("number1").value;
+    number2 = document.getElementById ("number2").value;
+    actual_answer= parseInt(number1) * parseInt(number2)
+}
